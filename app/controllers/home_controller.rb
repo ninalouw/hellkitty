984d1797d913f091496842dc9c868509cc4e2832
Nina Louw <ninalouw@mac.com>
2017-11-12 08:00:25 -0800

    create static about, contact and store pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+
+    def about_ian
+        @title = "Ian Boothby"
+        render :about_ian
+    end
+
+    def about_pia
+        @title = "Pia Guerra"
+        render :about_pia
+    end
+
+    def contact
+        render :contact
+    end
+
+    def store
+        render :store
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'announcements#index'
+  # announcements
   get "/announcements/new" => "announcements#new", as: :new_announcement
   post "/announcements" => "announcements#create", as: :announcements
   get "/announcements/:id" => "announcements#show", as: :announcement
@@ -9,6 +10,12 @@ Rails.application.routes.draw do
   patch "/announcements/:id" => "announcements#update"
   delete "/announcements/:id" => "announcements#destroy"
 
+  # static pages
+  get '/about-ian' => 'home#about_ian'
+  get '/about-pia' => 'home#about_pia'
+  get '/store' => 'home#store'
+  get '/contact' => 'home#contact'
+  # users
   resources :users, only: [:new, :create]
     resources :sessions, only: [:new, :create] do
       delete :destroy, on: :collection
