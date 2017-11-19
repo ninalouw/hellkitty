bb77c7edc7e1c02d1acd94c9be9b8edf3287a4bb
Nina Louw <ninalouw@mac.com>
2017-11-19 13:04:32 -0800

    remove old js

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -4,10 +4,4 @@ $(function () {
         $('#Modal').show().modal();
         $("html").unbind("mouseleave");
     });
-
-    // $(".navbar-toggler").click(function () {
-    //     //This code is called when navbar-mobile-toggle is clicked
-    //     $(this).toggleClass("is-open");
-    //     $(".navbar-collapse").toggleClass("is-open");
-    // });
 });
