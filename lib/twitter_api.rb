cc2b77f04a1bdb6fd1d7d2f03dffe89f64f641d1
Nina Louw <ninalouw@mac.com>
2017-11-21 19:47:39 -0800

    change twitter vars

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -9,8 +9,8 @@ class TwitterApi
 
     def self.client
         @client ||= Twitter::REST::Client.new do |config|
-            config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-            config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+            config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
+            config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
         end
     end
 end
