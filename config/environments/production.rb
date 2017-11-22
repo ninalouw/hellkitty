1c118e38a2283365fb6da3475840b014bdac8f53
Nina Louw <ninalouw@mac.com>
2017-11-21 20:05:58 -0800

    add redis for caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.cache_store = :redis_store if ENV['REDIS_URL']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
